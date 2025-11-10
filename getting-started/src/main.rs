5bb407afd1e1a2908ecdc063080daf9a161cf7c0
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-11-10 14:45:58 +0700

    chore: getting started from https://rust-lang.org/learn/get-started/

---
+++ getting-started/src/main.rs
@@ -0,0 +1,12 @@
+use ferris_says::say;
+use std::io::{BufWriter, stdout};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello! I'm Anggit and I love Putri <3");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+
+    say(&message, width, &mut writer).unwrap();
+}
